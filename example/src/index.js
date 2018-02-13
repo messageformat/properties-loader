859e5822a61b13a30ba80ff6a0465d39b41ef006
Eemeli Aro <eemeli@gmail.com>
2018-02-13 15:19:32 +0200

    Add example

---
+++ example/src/index.js
@@ -0,0 +1,14 @@
+import messages from './messages.properties'
+const { format, messages: errors } = messages.errors
+
+function component() {
+  const element = document.createElement('div')
+  element.innerHTML = format([
+    'Your message',
+    errors.wrong_length({ count: 42 })
+  ])
+  return element
+}
+
+console.log('messages', messages)
+document.body.appendChild(component())
---
+++ example/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.properties$/,
+        loader: require.resolve('messageformat-properties-loader'),
+        options: {
+          biDiSupport: false,
+          locales: 'en',
+          path: true
+        }
+      }
+    ]
+  }
+}
