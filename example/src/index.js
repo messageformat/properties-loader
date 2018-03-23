6d3859a3e356b26234f4cef7ca094b96f8f10201
Eemeli Aro <eemeli@gmail.com>
2018-03-23 18:00:24 +0200

    Update example for new API

--- example/src/index.js
+++ example/src/index.js
@@ -1,14 +1,15 @@
-import messages from './messages_en.properties'
-const { format, messages: errors } = messages.errors
+import en from './messages_en.properties'
+import MessageFormat from 'messageformat/lib/get'
+const messages = new MessageFormat({ en })
 
 function component() {
   const element = document.createElement('div')
-  element.innerHTML = format([
+  element.innerHTML = messages.get(['errors', 'format'], [
     'Your message',
-    errors.wrong_length({ count: 42 })
+    messages.get(['errors', 'messages', 'wrong_length'], { count: 42 })
   ])
   return element
 }
 
-console.log('messages', messages)
+console.log('messages', messages.get([]))
 document.body.appendChild(component())
